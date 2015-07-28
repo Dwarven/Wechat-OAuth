6e50deb5cc3e6c095e831f3914c24aa30ed43572
Dwarven YANG <prison.yang@gmail.com>
2015-07-28 12:12:50 +0800

    init project

---
+++ Wechat-OAuth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wechat-OAuth
+//
+//  Created by Dwarven on 15/7/28.
+//  Copyright (c) 2015年 Dwarven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Wechat-OAuth/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Wechat-OAuth
+//
+//  Created by Dwarven on 15/7/28.
+//  Copyright (c) 2015年 Dwarven. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
