0be1c7fe4bb26bdd9f916068f4c24034bce78e10
Dwarven YANG <prison.yang@gmail.com>
2015-09-16 12:58:19 +0800

    Code Optimization for Image Compression

--- Wechat-OAuth/WechatAccess.m
+++ Wechat-OAuth/WechatAccess.m
@@ -12,6 +12,72 @@
 #define WECHAT_APP_ID         @"yourappid"
 #define WECHAT_APP_SECRET     @"yourappsecret"
 
+@interface UIImage (DY)
+
+- (UIImage *)DY_thumbnailWithSize:(CGSize)asize;
+
+@end
+
+@implementation UIImage (DY)
+
+- (UIImage *)DY_thumbnailWithSize:(CGSize)asize{
+    
+    UIImage *newimage;
+    
+    if (nil == self) {
+        
+        newimage = nil;
+        
+    } else {
+        
+        CGSize oldsize = self.size;
+        
+        CGRect rect;
+        
+        if (asize.width/asize.height > oldsize.width/oldsize.height) {
+            
+            rect.size.width = asize.height*oldsize.width/oldsize.height;
+            
+            rect.size.height = asize.height;
+            
+            rect.origin.x = (asize.width - rect.size.width)/2;
+            
+            rect.origin.y = 0;
+            
+        } else {
+            
+            rect.size.width = asize.width;
+            
+            rect.size.height = asize.width*oldsize.height/oldsize.width;
+            
+            rect.origin.x = 0;
+            
+            rect.origin.y = (asize.height - rect.size.height)/2;
+            
+        }
+        
+        UIGraphicsBeginImageContext(asize);
+        
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        
+        CGContextSetFillColorWithColor(context, [[UIColor clearColor] CGColor]);
+        
+        UIRectFill(CGRectMake(0, 0, asize.width, asize.height));//clear background
+        
+        [self drawInRect:rect];
+        
+        newimage = UIGraphicsGetImageFromCurrentImageContext();
+        
+        UIGraphicsEndImageContext();
+        
+    }
+    
+    return newimage;
+    
+}
+
+@end
+
 @interface WechatAccess ()<WXApiDelegate>{
     void(^_result)(BOOL,id);
     void(^_shareResult)(BOOL,id);
@@ -120,10 +186,10 @@
     message.description = description;
     
     CGFloat compressionQuality = 1.f;
-    NSData * imageData = UIImageJPEGRepresentation(image, compressionQuality);
+    NSData * imageData = UIImageJPEGRepresentation(image, 1.f);
     while (imageData.length > 32768) {
         compressionQuality *= 0.5;
-        imageData = UIImageJPEGRepresentation(image, compressionQuality);
+        imageData = UIImageJPEGRepresentation([image DY_thumbnailWithSize:CGSizeMake(image.size.width * compressionQuality, image.size.height * compressionQuality)], 1.f);
     }
     
     [message setThumbData:imageData];
